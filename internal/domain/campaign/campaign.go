a6589fd5059cf3cdc7162bbe188888ed5840629b
Eddie <edmilson_jb@hotmail.com>
2025-09-28 19:11:08 -0300

    fix/-> campaign struct

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -2,10 +2,14 @@ package campaign
 
 import "time"
 
+type Contact struct {
+	Email string
+}
+
 type Campaign struct {
 	ID          int
 	Name 			string
 	CreatedOn time.Time
 	Content   string
-	Contacts []string
+	Contacts []Contact
 }
