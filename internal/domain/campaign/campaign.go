0c31cc6c2707b9397332f863ac20798129af0149
Eddie <edmilson_jb@hotmail.com>
2025-09-28 19:22:23 -0300

    feat/-> create newCampaingn

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -7,9 +7,23 @@ type Contact struct {
 }
 
 type Campaign struct {
-	ID          int
-	Name 			string
-	CreatedOn time.Time
-	Content   string
-	Contacts []Contact
+	ID          string
+	Name 				string
+	CreatedOn 	time.Time
+	Content   	string
+	Contacts 		[]Contact
+}
+
+func NewCampaign(name string, content string, emails []string) *Campaign {
+	contacts := make([]Contact, len(emails))
+	for i, email := range emails {
+		contacts[i].Email = email
+	}
+	return &Campaign{
+		ID: 				"1",
+		Name:      	name,
+		CreatedOn: 	time.Now(),
+		Content:   	content,
+		Contacts: 	contacts,
+	}
 }
