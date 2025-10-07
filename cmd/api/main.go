d6938b408e3464abad7e907d1dc76649a7393270
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2025-10-06 21:46:12 -0300

    feat: create post campaign

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,34 @@ package main
 import (
 	"net/http"
 
+	"github.com/edmilsonmedeiross/emailn/internal/contract"
+	"github.com/edmilsonmedeiross/emailn/internal/domain/campaign"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to the API"))
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+
+	service := campaign.Service{}
+
+	r.Post("/campaigns", func(w http.ResponseWriter, r *http.Request) {
+		var request contract.NewCampaignDTO
+		render.DecodeJSON(r.Body, &request)
+
+		id, err := service.Create(request)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		render.Status(r, http.StatusCreated)
+		render.JSON(w, r, map[string]string{"id": id})
 	})
 
 	http.ListenAndServe(":8080", r)
